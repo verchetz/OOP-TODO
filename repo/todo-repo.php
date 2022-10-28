cb4c2542c942c3f485898b17255a593922593fd5
verchetz <verchetz@gmail.com>
2022-10-28 11:08:56 +0700

    menambahkan implements pada repo dan servis

--- repo/todo-repo.php
+++ repo/todo-repo.php
@@ -4,8 +4,25 @@
 namespace Repo{
     use Entity\Todo;
     interface TodoRepo{
-        function save(Todo $todo):void;
-        function remove($num):bool;
-        function getAll():array;
+        function save(Todo $todo);
+        function remove($num);
+        function getAll();
+    }
+
+    class TodoRepoImpl implements TodoRepo{
+
+        private $todo = [];
+
+        function save(Todo $todo){
+
+        }
+
+        function remove($num){
+
+        }
+
+        function getAll(){
+            return $this->todo;
+        }
     }
 }
--- service/Todo-svc.php
+++ service/Todo-svc.php
@@ -2,10 +2,41 @@
 
 namespace Service{
 
+    use Repo\TodoRepo;
+
     interface TodoService{
         function showTodo();
-        function addTodo($todo):void;
-        function rmvTodo($num):void;
+        function addTodo($todo);
+        function rmvTodo($num);
+    }
+
+    class TodoSvcImpl implements TodoService{
+
+        private TodoRepo $todoRepo;
+
+        public function __construct(TodoRepo $todoRepo)
+        {
+            $this->todoRepo = $todoRepo;
+        }
+
+        function showTodo(){
+
+            echo "TODOLIST \n";
+
+            $todo = $this->todoRepo->getAll();
+
+            foreach($todo as $key => $val){
+                echo "$key. $val \n";
+            }
+        }
+
+        function addTodo($todo){
+
+        }
+
+        function rmvTodo($num){
+
+        }
     }
 
 }
