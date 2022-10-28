01fbdefe09a137b1c8deddf50b1675e84362159d
verchetz <verchetz@gmail.com>
2022-10-28 11:59:55 +0700

    membuat tes

---
+++ test/Todo-svc-tes.php
@@ -0,0 +1,19 @@
+<?php
+
+use Service\TodoSvcImpl;
+use Repo\TodoRepoImpl;
+
+require_once __DIR__ . "/../service/Todo-svc.php";
+require_once __DIR__ . "/../repo/Todo-repo.php";
+require_once __DIR__ . "/../entity/Todo.php";
+
+
+
+function tesShowTodo(){
+    $todoRepo = new TodoRepoImpl();
+    $todoSvc = new TodoSvcImpl($todoRepo);
+
+    $todoSvc->showTodo();
+}
+
+tesShowTodo();
