8b9ca288cc88c77fdbc2b5c74288df735e410a7d
verchetz <verchetz@gmail.com>
2022-11-01 10:24:21 +0700

    bug fixin todo-repo

--- repo/todo-repo.php
+++ repo/todo-repo.php
@@ -11,18 +11,56 @@ namespace Repo{
 
     class TodoRepoImpl implements TodoRepo{
 
-        private $todo = [];
+        public $todo = [];
 
         function save(Todo $todo){
-
+            $nums = count($this->todo) + 1;
+            $this->todo[$nums] = $todo;
         }
 
         function remove($num){
 
+            if($num > count($this->todo)){
+                return false;
+            }
+        
+            for($i = $num; $i < count($this->todo); $i++){
+                 $this->todo[$i] = $this->todo[$i + 1];
+            }
+        
+            unset($this->todo[count($this->todo)]);
+        
+            return true;
         }
 
         function getAll(){
             return $this->todo;
         }
     }
+
+    echo "DUMB";
+    echo "DUMB";
+    echo "DUMB";
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
