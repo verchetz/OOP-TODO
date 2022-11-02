500cc4e56438940599eeb4c3b375e6d7aea88286
verchetz <verchetz@gmail.com>
2022-11-02 11:48:08 +0700

    commit todoview bcz somehow its not fking committed

--- view/todo-view.php
+++ view/todo-view.php
@@ -2,10 +2,42 @@
 
 namespace View{
 
+    use Service\TodoService;
+    use Helper\Input;
+
     class TodoView{
+
+
+        private TodoService $todoService;
+
+        public function __construct(TodoService $todoService)
+        {
+            $this->todoService = $todoService;
+        }
+
         public function showTodo()
         {
-            # code...
+            while(true){
+                $this->todoService->showTodo();
+        
+                echo "Menu \n";
+                echo "1. Tambah list \n";
+                echo "2. Hapus Todo \n";
+                echo "x. keluar \n";
+            
+                $pilihan = Input::input("pilih");
+            
+                if ($pilihan == "1") {
+                    $this->addTodo();
+                }else if($pilihan == "2"){
+                    $this->rmvTodo();
+                }elseif($pilihan == "x"){
+                    break;
+                }else{
+                    echo "Harap masukan input sesuai menu \n";
+                }
+            }
+            echo "BABAI \n";
         }
 
         public function addTodo()
